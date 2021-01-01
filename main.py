dc24c63a92776dd178022ab8e706cb4ec9fe5c24
Ntchinda-Giscard <ntchinda1998@gmail.com>
2021-01-01 03:09:36 +0100

    added pipline model evaluation to main and ran

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from mlProject.pipeline.stage_01_data_ingestion import DataingestionPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidtionPipeline
 from mlProject.pipeline.stage_03_data_transformation import DataTransformationPipeline
 from mlProject.pipeline.stage_04_model_trainer import ModelTrainerPipeline
+from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationPiepline
 
 STAGE_NAME = "Data ingestion"
 
@@ -45,6 +46,17 @@ STAGE_NAME="Model trainer"
 try:
     logger.info(f">>>>> Stage {STAGE_NAME} has started <<<<<")
     obj = ModelTrainerPipeline()
+    obj.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} has completed \n\n x=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model evaluation"
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} has started <<<<<")
+    obj = ModelEvaluationPiepline()
     obj.main()
     logger.info(f">>>>> Stage {STAGE_NAME} has completed \n\n x=========x")
 except Exception as e:
