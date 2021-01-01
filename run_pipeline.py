c3d48641a0894a3dce18a3b5bc79e494172da535
Ntchinda-Giscard <ntchinda1998@gmail.com>
2021-01-01 03:28:30 +0100

    training pipeline

---
+++ run_pipeline.py
@@ -0,0 +1,5 @@
+from mlProject.pipeline.training_pipeline import training_pipeline
+
+
+if __name__ == "__main__":
+    training_pipeline()
---
+++ src/mlProject/pipeline/training_pipeline.py
@@ -0,0 +1,15 @@
+from zenml import pipeline
+from mlProject.pipeline.stage_01_data_ingestion import data_ingestion
+from mlProject.pipeline.stage_02_data_validation import data_validation
+from mlProject.pipeline.stage_03_data_transformation import data_transformation
+from mlProject.pipeline.stage_04_model_trainer import model_trainer
+from mlProject.pipeline.stage_05_model_evaluation import model_evaluation
+
+
+@pipeline
+def training_pipeline():
+    data_ingestion()
+    data_validation()
+    data_transformation()
+    model_trainer()
+    model_evaluation()
