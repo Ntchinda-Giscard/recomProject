127426e755aa330dfa7a39048ce2d5cb66bb11b6
Ntchinda-Giscard <ntchinda1998@gmail.com>
2021-01-01 06:10:26 +0100

    ignore artifacts

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataingestionPipeline
 
-logger.info("This is an example logging")
+
+STAGE_NAME = "Data ingestion"
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} has started <<<<<")
+    obj = DataingestionPipeline()
+    obj.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} has completed \n\n x=========x")
+except Exception as e:
+    raise e
