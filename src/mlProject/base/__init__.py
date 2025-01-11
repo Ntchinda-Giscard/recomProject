3ec20bb9c8d3befa1cb3a253159714784e1cbb7a
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-01-11 21:46:48 +0100

    experiment with data validation steps

--- src/mlProject/base/__init__.py
+++ src/mlProject/base/__init__.py
@@ -1,13 +1,27 @@
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
 import pandas as pd
+from typing import Any
 
 class DataLoader(ABC):
 
     @abstractmethod
-    def download_data(self) -> pd.DataFrame:
+    def download_data(self) -> None:
         pass
 
     @abstractmethod
     def extrat_zip_file(self) -> None:
+        pass
+
+
+class DataValidator(ABC):
+
+    @abstractmethod
+    def validate(self) -> Any:
+        pass
+
+class DataProfiler(ABC):
+
+    @abstractmethod
+    def data_profiler(self) -> Any:
         pass
