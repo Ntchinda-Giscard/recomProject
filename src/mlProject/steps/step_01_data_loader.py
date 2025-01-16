48c7dfa57cd6282d5742850cfcf02fb8ec38bf53
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-01-17 00:33:38 +0100

    added color to our prints

--- src/mlProject/steps/step_01_data_loader.py
+++ src/mlProject/steps/step_01_data_loader.py
@@ -8,7 +8,7 @@ from pathlib import Path, PosixPath
 
 
 
-STAGE_NAME = "Data ingestion stage"
+STAGE_NAME = "Data loader stage"
 
 class DataingestionPipeline:
 
@@ -27,10 +27,10 @@ class DataingestionPipeline:
 @step(output_materializers = PosixPathMaterializer, enable_cache=False)
 def data_loader() -> PosixPath:
     try:
-        logger.info(f">>>>>1️⃣ Stage {STAGE_NAME} has started 🏁🏁<<<<<")
+        logger.info(f"\33[33m >>>>> 1️⃣ Stage {STAGE_NAME} has started 🏁🏁 <<<<<\33[0m")
         obj = DataingestionPipeline()
         path = obj.main()
-        logger.info(f">>>>> Stage {STAGE_NAME} has completed ✅x=========x")
+        logger.info(f"\33[33m>>>>> Stage {STAGE_NAME} has completed ✅x=========x\33[0m")
         return PosixPath(path)
     except Exception as e:
         logger.exception(f"Oops😟! An error occured: {e} ")
