b00e610535b5973a0c8a2479cc2084b941686508
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-01-17 02:20:09 +0100

    better logging

--- src/mlProject/components/data_processing.py
+++ src/mlProject/components/data_processing.py
@@ -155,7 +155,7 @@ class DataPreprocessor:
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         logger.info(f"Training set size: {X_train.shape}")
         logger.info(f"Testing set size: {X_test.shape}")
-        logger.info(f"Spliting Final dataset \033[34mcompleted\033[0m ✅")
+        logger.info(f"Spliting \033[36mfinal dataset\033[0m \033[34mcompleted\033[0m ✅")
 
         return X_train, X_test, y_train, y_test
 
