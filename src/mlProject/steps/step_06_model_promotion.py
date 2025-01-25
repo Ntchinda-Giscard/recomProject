ea1b162348a01c08217bc257db653a89767ad414
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-01-25 07:51:52 +0100

    update model promoter

--- src/mlProject/steps/step_06_model_promotion.py
+++ src/mlProject/steps/step_06_model_promotion.py
@@ -1,10 +1,14 @@
 from zenml import step, get_step_context
+from zenml.client import Client
+
 import bentoml
 from bentoml._internal.models.model import Model
 
 @step(enable_cache=False)
-def promote_to_bentoml() -> str:  # Explicit return type
-    model_version = get_step_context().model_version
-    model = model_version.get_artifact("recommend_model").load()
+def promote_to_bentoml() -> None:  # Explicit return type
+    latest_model_version = client.get_latest_model_version(name="RecommendNet")
+
+    print(f"Artifact {model_version}")
+    # model = model_version.get_artifact("recommend_model").load()
     # bentoml_model: Model = bentoml.sklearn.save_model("RecommendNet", model)
     # return bentoml_model.tag  # Returns a string like "zenml_promoted_model:latest"
